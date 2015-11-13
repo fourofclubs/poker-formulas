e91892b4b626e1df2170ba4434996e83543cb58e
Brad Micholson <brad_micholson@cnphi-rcrsp.ca>
2015-11-13 15:23:12 -0600

    Nat stuff.

--- .worksheet/src/ca.fourofclubs.utils.math.scratch.scala
+++ .worksheet/src/ca.fourofclubs.utils.math.scratch.scala
@@ -2,8 +2,13 @@ package ca.fourofclubs.utils.math
 import Complex._
 import Math._
 import Rational._
+import Nat._
 
-object scratch {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(176); 
+object scratch {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(189); 
   val e1 = Prod(Sum(Val(1), Sum(Var("x"), Val(8))), Sum(Var("y"), Val(5)));System.out.println("""e1  : ca.fourofclubs.utils.math.Prod = """ + $show(e1 ));$skip(30); val res$0 = 
-  e1.eval("x" -> 3, "y" -> 2);System.out.println("""res0: Double = """ + $show(res$0))}
+  e1.eval("x" -> 3, "y" -> 2);System.out.println("""res0: Double = """ + $show(res$0));$skip(21); val res$1 = 
+
+  Nat("10") * "11";System.out.println("""res1: ca.fourofclubs.utils.math.Nat = """ + $show(res$1));$skip(20); val res$2 = 
+
+  "199".successor;System.out.println("""res2: ca.fourofclubs.utils.math.Nat = """ + $show(res$2))}
 }
--- src/main/scala/ca/fourofclubs/utils/math/scratch.sc
+++ src/main/scala/ca/fourofclubs/utils/math/scratch.sc
@@ -2,9 +2,14 @@ package ca.fourofclubs.utils.math
 import Complex._
 import Math._
 import Rational._
+import Nat._
 
 object scratch {
   val e1 = Prod(Sum(Val(1), Sum(Var("x"), Val(8))), Sum(Var("y"), Val(5)))
                                                   //> e1  : ca.fourofclubs.utils.math.Prod = (1+x+8)*(y+5)
   e1.eval("x" -> 3, "y" -> 2)                     //> res0: Double = 84.0
+
+  Nat("10") * "11"                                //> res1: ca.fourofclubs.utils.math.Nat = 110
+
+  "199".successor                                 //> res2: ca.fourofclubs.utils.math.Nat = 200
 }
