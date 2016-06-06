af29b04553ead5eb86639e07380f856357cd546f
Brad Micholson <brad_micholson@cnphi-rcrsp.ca>
2016-06-06 11:24:39 -0500

    Fun stuff.

---
+++ .worksheet/src/ca.fourofclubs.playground.monads.mona.scala
@@ -0,0 +1,5 @@
+package ca.fourofclubs.playground.monads
+
+object mona {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(99); 
+  println("Welcome to the Scala worksheet")}
+}
