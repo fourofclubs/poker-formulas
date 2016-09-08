97634d2e13cf2a5d5787c4b762df9fbf591a1034
Brad Micholson <fourofclubs@gmail.com>
2016-09-07 21:44:43 -0500

    Removed worksheet stuff.

--- .worksheet/src/ca.fourofclubs.playground.db.Db.scala
+++
@@ -1,6 +0,0 @@
-package ca.fourofclubs.playground.db
-
-object Db {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(79); val res$0 = 
-	
-  UserDao("brad_micholson");System.out.println("""res0: ca.fourofclubs.playground.db.Query[Option[ca.fourofclubs.playground.db.User]] = """ + $show(res$0))}
-}
--- .worksheet/src/ca.fourofclubs.playground.facebook.scala
+++
@@ -1,7 +0,0 @@
-package ca.fourofclubs.playground
-
-import facebook4j.FacebookFactory
-
-object Facebook {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(142); 
-  val facebook = new FacebookFactory().getInstance();System.out.println("""facebook  : facebook4j.Facebook = """ + $show(facebook ))}
-}
